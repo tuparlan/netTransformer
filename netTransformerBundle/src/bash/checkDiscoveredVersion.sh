0e798f21035bed11bd9cf5227832c41a9fe96fef
Nick Milovanov <nmil@itransformers.net>
2016-12-04 21:18:35 +0200

    updating the verification scripts to work with linux xpath

--- netTransformerBundle/src/bash/checkDiscoveredVersion.sh
+++ netTransformerBundle/src/bash/checkDiscoveredVersion.sh
@@ -39,25 +39,25 @@ checkDirectedGraphml(){
 
 checkGraphml(){
     pwd;
-    keyNum=`cat network.graphml | xpath 'count(/graphml//key)' 2>/dev/null`;
+    keyNum=`cat network.graphml | xpath -e 'count(/graphml//key)' 2>/dev/null`;
 
     if [ -z "$keyNum" ]; then
        echo "Number of keys: $keyNum is empty!";
         exit 1;
     fi
 
-    graphNum=`cat network.graphml | xpath 'count(/graphml//graph)' 2>/dev/null`;
+    graphNum=`cat network.graphml | xpath -e 'count(/graphml//graph)' 2>/dev/null`;
     if [ -z "$graphNum" ]; then
        echo "There is no graphs in graphml!";
         exit 1;
     fi
 
-    nodeNum=`cat network.graphml | xpath 'count(/graphml//node)' 2>/dev/null`;
+    nodeNum=`cat network.graphml | xpath -e 'count(/graphml//node)' 2>/dev/null`;
     if [ -z "$nodeNum" ]; then
        echo "There is no nodes in graphml!";
         exit 1;
     fi
-    edgeNum=`cat network.graphml | xpath 'count(/graphml//edge)' 2>/dev/null`;
+    edgeNum=`cat network.graphml | xpath -e 'count(/graphml//edge)' 2>/dev/null`;
     if [ -z "$nodeNum" ]; then
        echo "There is no edges in graphml!";
         exit 1;
